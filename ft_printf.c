7946e2bed638ed263292f09600cd1c60eb88474d
appinha <amanda_pinha@hotmail.com>
2020-05-09 17:06:37 -0300

    Update for start testing

--- ft_printf.c
+++ ft_printf.c
@@ -6,32 +6,63 @@
 /*   By: apuchill <apuchill@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/06 15:10:37 by apuchill          #+#    #+#             */
-/*   Updated: 2020/05/07 00:03:39 by apuchill         ###   ########.fr       */
+/*   Updated: 2020/05/09 16:51:36 by apuchill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+/*
+static int	ft_strchr_specs(char *s, char c)
+{
+	int i;
 
-int	ft_printf(const char *str, ...)
+	i = 0;
+	while (terms[i] != '\0')
+	{
+		if (terms[i == c])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+static int	get_flags(va_list args, const char *format, int len, int *i)
+{
+	int j;
+
+	j = 0;
+	if (format[*i] != '%')
+	{
+		while (ft_strchr_specs(ALL_SPEC, format[*i]))
+		{
+
+		}
+	}
+	return (len);
+}
+*/
+static int	print_simple(char c, int len)
+{
+	//ft_putchar(c);
+	return (c + len + 1);
+}
+
+int			ft_printf(const char *format, ...)
 {
 	va_list	args;
 	int		len;
 	int		i;
 
-	va_start(args, str);
+	va_start(args, format);
 	len = 0;
 	i = 0;
-	while (str[i] != '\0')
+	while (format[i] != '\0')
 	{
-		if (str[i] != '%')
-		{
-			ft_putchar(str[i]);
-			len++;
-			i++;
-
-		}
+		if (format[i] != '%')
+			len = print_simple(format[i++], len);
 		else
 		{
+			//len = get_flags(args, format, len, &i)
 			len++;
 			i += 2;
 		}
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: apuchill <apuchill@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/06 15:12:29 by apuchill          #+#    #+#             */
-/*   Updated: 2020/05/04 15:31:17 by apuchill         ###   ########.fr       */
+/*   Updated: 2020/05/09 12:25:57 by apuchill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,44 @@
 # include <stdlib.h>
 # include <stdarg.h>
 
+/*
+** FLAG COLLECTION STRUCT
+** '-' (minus): justification
+** '0' (zero): padding
+** '.' (dot): precision
+** '*' (star): argument (complements other flags)
+** width: min number of char to be printed
+*/
+
+typedef struct	s_flags
+{
+	int minus;
+	int zero;
+	int dot;
+	int star;
+	int width;
+}				t_flags;
+
+/*
+** MACROS
+*/
+
+# define SPECS		"cspdiuxX%"
+# define SPECS_B	"nfge"
+# define FLAGS		"-0.*123456789"
+# define FLAGS_B	"l ll h hh # +"
+# define ALL_SPEC	SPECS FLAGS
+# define HEXA_LOW	"0123456789abcdef"
+# define HEXA_UP	"0123456789ABCDEF"
+
+/*
+** MAIN FUNCTIONS
+*/
+
 int	ft_printf(const char *str, ...);
 
 /*
-** Utils
+** UTILS FUNCTIONS
 */
 
 size_t	ft_strlen(const char *s);
