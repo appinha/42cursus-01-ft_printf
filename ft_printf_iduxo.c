00e9ebf68ccf72e4b275e9eb20060df4dfe28411
appinha <amanda_pinha@hotmail.com>
2020-05-30 23:27:42 -0300

    Corrected width and 0 case in %o (2)

--- ft_printf_iduxo.c
+++ ft_printf_iduxo.c
@@ -6,7 +6,7 @@
 /*   By: apuchill <apuchill@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/17 13:25:14 by apuchill          #+#    #+#             */
-/*   Updated: 2020/05/30 22:52:50 by apuchill         ###   ########.fr       */
+/*   Updated: 2020/05/30 23:26:24 by apuchill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,9 +70,11 @@ void	print_spec_o(int *len, t_flags fl, va_list args)
 	if (fl.length >= 2)
 		fl.ulli = va_arg(args, unsigned long long int);
 	fl.sign = 'u';
-	fl.print_n0 = 0;
-	//if (fl.ulli == 0)
-	//	fl.hash = 0;
+	fl.print_n0 = 1;
+	if (fl.point == 1 && fl.precision == 0 && fl.hash == 0)
+		fl.print_n0 = 0;
+	if (fl.ulli == 0)
+		fl.hash = 0;
 	fl.a = ft_ullitoa_base(fl.ulli, OCTAL);
 	print_flags(len, fl);
 	free(fl.a);
